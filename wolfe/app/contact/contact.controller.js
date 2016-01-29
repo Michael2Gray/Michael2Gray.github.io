5b602d3fbb4e2c9d197701cd64be1daa190a758c
Michael Gray <michael2.gray@gmail.com>
2016-01-29 22:46:46 +0000

    Revert "First Wolfe Instance"
    This reverts commit ab4c3268da1efc51c82df3e0630c1b5a9a2c3568.

--- wolfe/app/contact/contact.controller.js
+++
@@ -1,9 +0,0 @@
-(function(window, $, angular, undefined) {
-	'use strict';
-	angular.module('wolfe.contact')
-
-	.controller('ContactCtrl', ['$scope', function ($scope) {
-		console.log('IM Contact');
-	}])
-	;
-})(window, window.$, window.angular);
--- wolfe/app/services/services.controller.js
+++
@@ -1,9 +0,0 @@
-(function(window, $, angular, undefined) {
-	'use strict';
-	angular.module('wolfe.services')
-
-	.controller('ServicesCtrl', ['$scope', function ($scope) {
-		console.log('IM Services');
-	}])
-	;
-})(window, window.$, window.angular);
