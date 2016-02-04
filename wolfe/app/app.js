5246080cb0f0116f91ceb6c57b65639155d667d1
Michael Gray <michael2.gray@gmail.com>
2016-02-04 21:50:26 +0000

    1. Fixed Template View URls

--- wolfe/app/app.js
+++ wolfe/app/app.js
@@ -39,27 +39,27 @@
 	.config(function ($routeProvider){
 		$routeProvider.
 			when('/about', {
-				templateUrl: '/app/about/about.tpl.html',
+				templateUrl: './wolfe/app/about/about.tpl.html',
 				controller: 'AboutCtrl'
 			}).
 			when('/brands', {
-				templateUrl: '/app/brands/brands.tpl.html',
+				templateUrl: './wolfe/app/brands/brands.tpl.html',
 				controller: 'BrandsCtrl'
 			}).
 			when('/contact', {
-				templateUrl: '/app/contact/contact.tpl.html',
+				templateUrl: './wolfe/app/contact/contact.tpl.html',
 				controller: 'ContactCtrl'
 			}).
 			when('/cycle-to-work', {
-				templateUrl: '/app/cycleToWork/cycleToWork.tpl.html',
+				templateUrl: './wolfe/app/cycleToWork/cycleToWork.tpl.html',
 				controller: 'CycleToWorkCtrl'
 			}).
 			when('/home', {
-				templateUrl: '/app/home/home.tpl.html',
+				templateUrl: './wolfe/app/home/home.tpl.html',
 				controller: 'HomeCtrl'
 			}).
 			when('/services', {
-				templateUrl: '/app/services/services.tpl.html',
+				templateUrl: './wolfe/app/services/services.tpl.html',
 				controller: 'ServicesCtrl'
 			}).
 			otherwise({
